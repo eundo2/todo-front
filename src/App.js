49704211b66b2ceca8c22df18324341a222ca13d
eundo2 <ehdms2324@naver.com>
2023-03-31 10:17:26 +0900

    8. props, state 사용하기

--- src/App.js
+++ src/App.js
@@ -1,12 +1,19 @@
 import React from 'react';
-import Hello from './Hello';
+import Todo from './Todo';
 import './App.css';
 
 class App extends React.Component {
+  constructor(props) {  
+    super(props);
+    this.state = { 
+      item:{id:0, title:"Hello World1", done:true }
+    };
+  }
+  
   render() {
     return(
       <div className="App">
-        <Hello />
+        <Todo item={this.state.item}/>
       </div>
     );
   }
