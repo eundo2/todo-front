7914b9b3ce4819674760b4630e745eb441bfb1a8
eundo2 <ehdms2324@naver.com>
2023-03-31 14:47:29 +0900

    11. react로 목록 추가, 수정, 삭제하기

--- src/Todo.js
+++ src/Todo.js
@@ -1,28 +1,71 @@
-import { Checkbox, InputBase, ListItem, ListItemText } from "@material-ui/core";
+import { Checkbox, IconButton, InputBase, ListItem, ListItemSecondaryAction, ListItemText } from "@material-ui/core";
+import DeleteOutlined from "@material-ui/icons/DeleteOutlined";
 import React from "react";
 
 class Todo extends React.Component {
     constructor(props) {
         super(props);
-        this.state = { item:props.item };
+        this.state = { item:props.item, readOnly:true };
+        this.delete = props.delete;
+    }
+
+    deleteEventHandler = () => {
+        this.delete(this.state.item);
+    }
+    
+    offReadOnlyMode = () => {
+        this.setState({readOnly:false}, () => {
+            console.log("readonly", this.state.readOnly);
+        });
+    }
+    
+    enterKeyEventHandler = (e) => {
+        if(e.key === "Enter"){
+            this.setState({readOnly:true})
+        }
+    }
+    
+    editEventHandler = (e) => {
+        const thisItem = this.state.item;
+        thisItem.title = e.target.value;
+        this.setState({item:thisItem});
+    }
+    
+    checkboxEventHandler = (e) => {
+        const thisItem = this.state.item;
+        thisItem.done = thisItem.done? false:true;
+        this.setState({item:thisItem});
     }
 
     render() {
         const item = this.state.item;
         return(
             <ListItem>
-                <Checkbox checked={item.done} />
+                <Checkbox 
+                    checked={item.done}
+                    onChange={this.checkboxEventHandler} 
+                />
                 <ListItemText>
-                    <InputBase 
-                        inputProps={{"aria-label":"naked"}}
+                    <InputBase
+                        inputProps={{"aria-label":"naked", readOnly:this.state.readOnly}}
                         type="text"
                         id={item.id}
                         name={item.id}
                         value={item.title}
                         multiline={true}
                         fullWidth={true}
+                        onClick={this.offReadOnlyMode}
+                        onChange={this.editEventHandler}
+                        onKeyPress={this.enterKeyEventHandler}
                     />
                 </ListItemText>
+
+                <ListItemSecondaryAction>
+                    <IconButton aria-label="Delete"
+                        onClick={this.deleteEventHandler}>
+                            <DeleteOutlined />
+                    </IconButton>
+                </ListItemSecondaryAction>
             </ListItem>
         );
     }
