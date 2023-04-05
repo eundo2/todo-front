1af6682f21c0d08c1fac9a5588a81f09583a7d73
eundo2 <ehdms2324@naver.com>
2023-04-05 16:46:41 +0900

    문제있는 16번

--- src/App.js
+++ src/App.js
@@ -1,15 +1,16 @@
 import React from 'react';
 import Todo from './Todo';
-import './App.css';
-import { Container, List, Paper } from '@material-ui/core';
 import AddTodo from './AddTodo';
-import { call } from './service/ApiService';
+import { AppBar, Button, Container, Grid, List, Paper, Toolbar, Typography } from '@material-ui/core';
+import './App.css';
+import { call, signout } from './service/ApiService';
 
 class App extends React.Component {
   constructor(props) {  
     super(props);
     this.state = {
       items: [],
+      loading: true,
     };
   }
 
@@ -33,7 +34,7 @@ class App extends React.Component {
   
   componentDidMount() {
     call("/todo", "GET", null).then((response) =>
-      this.setState({items:response.data})
+      this.setState({items:response.data, loading:false})
     );
   }
   
@@ -42,18 +43,49 @@ class App extends React.Component {
       <Paper style={{margin:16}}>
         <List>
           {this.state.items.map((item, idx) => (
-            <Todo item={item} key={item.id} delete={this.delete} />
+            <Todo item={item} key={item.id} delete={this.delete} update={this.update}/>
           ))}
         </List>
       </Paper>
     );
 
-    return(
-      <div className='App'>
+    var navigationBar = (
+      <AppBar position='static'>
+        <Toolbar>
+          <Grid justify='space-between' container>
+            <Grid item>
+              <Typography variant='h6'>오늘의 할일</Typography>
+            </Grid>
+            <Grid item>
+              <Button color='inherit' onClick={signout}>logout
+              </Button>
+            </Grid>
+          </Grid>
+        </Toolbar>
+      </AppBar>
+    );
+
+    var todoListPage = (
+      <div>
+        {navigationBar}
         <Container maxWidth="md">
-          <AddTodo add={this.add}/>
+          <AddTodo add={this.add} />
           <div className='TodoList'>{todoItems}</div>
         </Container>
+      </div>
+    );
+
+    var loadingPage = <h1>로딩중...</h1>
+    var content = loadingPage;
+
+    if(!this.state.loading) {
+      console.log("loading end");
+      content = todoListPage;
+    }
+
+    return(
+      <div className='App'>
+        {content}
       </div>
     );
   }
--- src/service/ApiService.js
+++ src/service/ApiService.js
@@ -1,10 +1,19 @@
 import { API_BASE_URL } from "../app-config";
 
+const ACCESS_TOKEN = "ACCESS_TOKEN";
+
 export function call(api, method, request) {
+    let headers = new Headers({
+        "Content-Type":"application/json",
+    });
+    
+    const accessToken = localStorage.getItem("ACCESS_TOKEN");
+    if(accessToken) {
+        headers.append("Authorization", "Bearer "+accessToken);
+    }
+
     let options = {
-        headers: new Headers({
-            "Content-Type": "application/json",
-        }),
+        headers: headers,
         url: API_BASE_URL + api,
         method: method,
     };
@@ -20,10 +29,45 @@ export function call(api, method, request) {
                 return json;
             })
         ).catch((error) => {
+            console.log("Oops!");
             console.log(error.status);
+            console.log("Oops!");
             if(error.status === 403) {
                 window.location.href = "/login";
             }
             return Promise.reject(error);
-        });
+        }
+    );
+}
+
+export function signin(userDTO) {
+    return call("/auth/signin", "POST", userDTO)
+    .then((response) => {
+        if(response.token) {
+            localStorage.setItem("ACCESS_TOKEN", response.token);
+            window.location.href="/";
+        }
+    });
+}
+
+export function signup(userDTO) {
+    return call("/auth/signup", "POST", userDTO)
+    .then((response) => {
+        if(response.id) {
+            window.location.href="/";
+        }
+    }).catch((error) => {
+        console.log("Oops!");
+        console.log(error.status);
+        console.log("Oops!");
+        if(error.status === 403) {
+            window.location.href = "/auth/signup";
+        }
+        return Promise.reject(error);
+    });
+}
+
+export function signout() {
+    localStorage.setItem("ACCESS_TOKEN", null);
+    window.location.href="/";
 }
